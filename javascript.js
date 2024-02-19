8e5eba68434778c8e9368f80e23d609bfed9b50b
vamshikrishnasai <149854294+vamshikrishnasai@users.noreply.github.com>
2024-02-19 22:06:22 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,38 @@
+let box1=document.querySelector("#one");
+let box2=document.querySelector("#two");
+let box3=document.querySelector("#three");
+let box4=document.querySelector("#four");
+let img=document.querySelector("#image")
+
+box1.addEventListener("click",()=>{
+
+
+img.style.opacity="0.1";
+
+
+});
+    
+
+box2.addEventListener("click",()=>{
+
+
+    img.style.opacity="0.2";
+    
+  
+    });
+
+    box3.addEventListener("click",()=>{
+
+
+        img.style.opacity="0.5";
+        
+        
+        });
+        
+    box4.addEventListener("click",()=>{
+
+
+        img.style.opacity="1"
+        
+        
+        });
